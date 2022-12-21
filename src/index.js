7e8e9b0e37f92593870aba494d578406c12e3936
Marcela B Petroli <marcelabpetroli@gmail.com>
2022-12-21 12:51:35 +0100

    Generate unique id for each card, and create static server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const { uuid } = require('uuidv4');
 
 // create and config server
 const server = express();
 server.use(cors());
-server.use(express.json({limit: "25mb"}));
-
+server.use(express.json({ limit: '25mb' }));
 
 // init express aplication
 const serverPort = 4000;
@@ -13,30 +13,37 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-server.post('/card', (req, res) =>{
+const staticServerPath = './src/public-react';
+server.use(express.static(staticServerPath));
 
-  if( !req.body.name || !req.body.job || !req.body.email || !req.body.phone || !req.body.photo || !req.body.linkedin || !req.body.github ) {
-    // ERROR 
+const savedCards = [];
+
+server.post('/card', (req, res) => {
+  if (!req.body.name || !req.body.job || !req.body.email || !req.body.phone || !req.body.photo || !req.body.linkedin || !req.body.github) {
+    // ERROR
     const responseFalse = {
-      error: "Mandatory fields",
-      success: false
-    }
-    res.json(responseFalse);
-  }
-  else { 
-    const responseSuccess = {
-      cardURL: 'https://dev.adalab.es/card/8686837722797',
-      success: true
+      error: 'Mandatory fields',
+      success: false,
     };
-  res.json(responseSuccess);
+    res.json(responseFalse);
+  } else {
+    const newCard = {
+      id: uuid(),
+      ...req.body,
+    };
+
+    savedCards.push(newCard);
+
+    const responseSuccess = {
+      cardURL: `https://localhost:4000/card/${newCard.id}`,
+      success: true,
+    };
+    res.json(responseSuccess);
     // gGuardo la tarjeta y devuelvo success
   }
-
 });
 
-
-//express III
+//express III // motor de plantillas
 server.get('card/id', (req, res) => {
-//pintar la tarjeta
-
+  //pintar la tarjeta
 });
