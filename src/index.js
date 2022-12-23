14d727da83004a2371fbff567f9bb576c082364f
Cecilia Periquet <periwhat@gmail.com>
2022-12-23 14:01:11 +0100

    Create database, create template engine and css static server

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const { uuid } = require('uuidv4');
+const Database = require('better-sqlite3');
 
 // create and config server
 const server = express();
 server.use(cors());
 server.use(express.json({ limit: '25mb' }));
+server.set('view engine', 'ejs');
 
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+const db = new Database('./src/database/database.db', {
+  verbose: console.log,
 });
 
 const staticServerPath = './src/public-react';
@@ -19,7 +24,15 @@ server.use(express.static(staticServerPath));
 const savedCards = [];
 
 server.post('/card', (req, res) => {
-  if (!req.body.name || !req.body.job || !req.body.email || !req.body.phone || !req.body.photo || !req.body.linkedin || !req.body.github) {
+  if (
+    !req.body.name ||
+    !req.body.job ||
+    !req.body.email ||
+    !req.body.phone ||
+    !req.body.photo ||
+    !req.body.linkedin ||
+    !req.body.github
+  ) {
     // ERROR
     const responseFalse = {
       error: 'Mandatory fields',
@@ -44,6 +57,14 @@ server.post('/card', (req, res) => {
 });
 
 //express III // motor de plantillas
-server.get('card/id', (req, res) => {
-  //pintar la tarjeta
+server.get('/card/:id', (req, res) => {
+  const id = req.params.id;
+  const query = db.prepare('SELECT * FROM cards WHERE id = ?');
+  const userCard = query.get(id);
+
+  res.render('card', userCard);
+  console.log(userCard);
 });
+
+const staticServerCSSPath = './src/public-styles';
+server.use(express.static(staticServerCSSPath));
